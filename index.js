bca93b4396985524cfc77d215e2acbc82cc19715
AbhijeetVashishtha <abhijeetvashishth13>
2022-12-07 13:47:01 +0530

    Task-5

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 // console.dir(document);
 
+// GET ELEMENT BY ID
+
 //console.log(document.getElementById('header-title'));
-var headertitle = document.getElementById('header-title');
+// var headertitle = document.getElementById('header-title');
 // var header = document.getElementById('main-header');
 
 // console.log(headertitle);
@@ -13,22 +15,40 @@ var headertitle = document.getElementById('header-title');
 // console.log(headertitle.innerText);
 // headertitle.innerHTML = '<h3>hello</h3>';
 
-headertitle.style.borderBottom = 'Solid 3px #000';
+// headertitle.style.borderBottom = 'Solid 3px #000';
 
-var title = document.getElementsByClassName('title');
+// var title = document.getElementsByClassName('title');
 
-console.log(title);
-title[0].style.fontWeight = 'bold';
-title[0].style.color = 'green';
+// console.log(title);
+// title[0].style.fontWeight = 'bold';
+// title[0].style.color = 'green';
 
-var list = document.getElementsByClassName('list-group-item');
-console.log(list);
-list[2].style.backgroundColor = 'green';
+// GET ELEMENT BY CLASSNAME
+
+// var list = document.getElementsByClassName('list-group-item');
+// console.log(list);
+// list[2].style.backgroundColor = 'green';
+
+// // Gives Error
+// // list.style.fontWeight = 'bold';
+
+// for(var i =0;i<list.length;i++)
+// {
+//     list[i].style.fontWeight = 'bold';
+// }
+
+// GET ELEMENT BY TAGNAME
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+li[2].textContent = 'hello 3';
+li[2].style.fontWeight = 'bold';
+li[2].style.backgroundColor = 'green';
 
 // Gives Error
 // list.style.fontWeight = 'bold';
 
-for(var i =0;i<list.length;i++)
+for(var i =0;i<li.length;i++)
 {
-    list[i].style.fontWeight = 'bold';
+    li[i].style.fontWeight = 'bold';
 }
