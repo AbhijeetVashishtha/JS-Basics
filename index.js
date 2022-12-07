d856a796c20601f65d5c5b7b6c63ed4ed1671e90
AbhijeetVashishtha <abhijeetvashishth13>
2022-12-07 14:18:33 +0530

    Task-6 (QuerySelector)

--- index.js
+++ index.js
@@ -39,16 +39,39 @@
 
 // GET ELEMENT BY TAGNAME
 
-var li = document.getElementsByTagName('li');
-console.log(li);
-li[2].textContent = 'hello 3';
-li[2].style.fontWeight = 'bold';
-li[2].style.backgroundColor = 'green';
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// li[2].textContent = 'hello 3';
+// li[2].style.fontWeight = 'bold';
+// li[2].style.backgroundColor = 'green';
 
-// Gives Error
-// list.style.fontWeight = 'bold';
+// // Gives Error
+// // list.style.fontWeight = 'bold';
 
-for(var i =0;i<li.length;i++)
-{
-    li[i].style.fontWeight = 'bold';
-}
+// for(var i =0;i<li.length;i++)
+// {
+//     li[i].style.fontWeight = 'bold';
+// }
+
+// QuerySelector
+
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #000';
+
+var input = document.querySelector('input');
+input.value = "Hello World";
+
+var submit = document.querySelector('input[type ="submit"]');
+submit.value = "Send";
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastitem = document.querySelector('.list-group-item:last-child');
+lastitem.style.backgroundColor = 'yellow';
+
+var seconditem = document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.backgroundColor = 'green';
+
+var thirditem = document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.display = 'none';
