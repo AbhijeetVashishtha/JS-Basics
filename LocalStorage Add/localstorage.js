bbb48ea41de32741cfb9e1abf45545c6af8117db
AbhijeetVashishtha <82046366+AbhijeetVashishtha@users.noreply.github.com>
2022-12-09 12:02:38 +0530

    Delete LocalStorage Add directory

--- LocalStorage Add/localstorage.js
+++
@@ -1,9 +0,0 @@
-function saveToLocalStorage(event){
-    event.preventDefault();
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const number = event.target.number.value;
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
-    localStorage.setItem('phonenumber', number);
-}
